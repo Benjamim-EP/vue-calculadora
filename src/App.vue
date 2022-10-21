c8288fbea7f784f97a3a35ffbd6926fccc269b1d
Benjamim-EP <bannk18@gmail.com>
2022-10-21 17:51:31 -0300

    completo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Calculador</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator.vue"
+export default {
+  components: { Calculator }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-VariableFont_wght.ttf");
+}
+* {
+  font-family: "RobotoMono",monospace;
+}
+body {
+  margin:0;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color:rgb(8, 8, 8);
+  background-color: bisque;
+
+}
+</style>
